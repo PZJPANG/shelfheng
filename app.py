a54cef79827bd2d7c7245fbdf145ca8920e8f54d
JunBin <wongjb66@gmail.com>
2025-09-06 21:30:31 +0800

    first upload qianqian

---
+++ app.py
@@ -0,0 +1,99 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///shelfheng.db")
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 400)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        # Checking the validity of user
+        session.clear()
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+        if not username:
+            return apology("Username is required ", 400)
+        elif not password:
+            return apology("Password is required", 400)
+        elif not confirmation:
+            return apology("You must confirm your password", 400)
+        elif confirmation != password:
+            return apology("Password do not match", 400)
+        elif len(rows) != 0:
+            return apology("The user already exist", 400)
+        else:
+            # add new user into the database
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
+                       username, generate_password_hash(password))
+
+        # login the new user
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
+        session["user_id"] = rows[0]["id"]
+        return redirect("/")
+
+    return render_template("register.html")
